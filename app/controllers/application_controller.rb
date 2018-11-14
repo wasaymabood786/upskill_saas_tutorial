eb9c93340c88ce69b7b92b154c10c32d8d4698f1
Wasay Mabood <wasay.mabood@gmail.com>
2018-11-14 01:26:00 +0000

    Added code comments for Stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
 protect_from_forgery with: :exception
 
+  #If coming from a devise signup form
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
+    #Whitelist the following form fields for processing 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:stripe_card_token, :email, :password, :password_confirmation) }
     end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  #Extend default devise gem behavior so that
+  #users signing up with pro account (plan id 2)
+  #save with a special stripe subscription function
+  #otherwise, devise signs up user as usual
   def create
     super do |resource|
       if params[:plan]
